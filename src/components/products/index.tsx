33d8e65c9fe87202fde417aa64555a94dc97ff4d
Vietsalt <Vietprock@gmail.com>
2022-09-12 15:55:29 +0700

    delete upload img page

--- src/components/products/index.tsx
+++ src/components/products/index.tsx
@@ -8,32 +8,30 @@ import CampaignFrame from './step3/createCampaignForm'
 import CreateCampaignSuccess from './success'
 
 const ProductsPage = () => {
-  let title="";
-  const  currentTab = useSelector(selectProductCurrentPageState);
+	let title = "";
+	const currentTab = useSelector(selectProductCurrentPageState);
 	function generateTabs() {
-		switch(currentTab) {
-			case"1": return  <LogoUploadBox />
-			case "2": 
-				return <AddProduct />
-			
-			case "3": return<CreateCampaign />
-			case "3.1": return<CampaignFrame/>
-			case "success": return <CreateCampaignSuccess/>
+		switch (currentTab) {
+			case "1": return <LogoUploadBox />
+			case "2": return <AddProduct />
+			case "3": return <CreateCampaign />
+			case "3.1": return <CampaignFrame />
+			case "success": return <CreateCampaignSuccess />
 		}
 	}
-  switch(currentTab){
-	case"1": title= "Upload Logo";  break;
-	case"2": title= "Add product";  break;
-	case"3": title= "Create new Campaign"; break;
-	case"3.1": title= "Create new Campaign"; break;
-  }
-  return (
-	<>
-		<HeadContent currentTab={currentTab} title={title} />
+	switch (currentTab) {
+		case "1": title = "Upload Logo"; break;
+		case "2": title = "Add product"; break;
+		case "3": title = "Create new Campaign"; break;
+		case "3.1": title = "Create new Campaign"; break;
+	}
+	return (
+		<>
+			<HeadContent currentTab={currentTab} title={title} />
 			{generateTabs()}
-		
-	</>
-  )
+
+		</>
+	)
 }
 
 export default ProductsPage
